11fa074c65824cca8d58fb2f166a750c3f2a242d
Shakib D shy <shakibdshy@gmail.com>
2022-06-02 01:57:05 +0600

    initial File setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#0ff',
+    backgroundColor: '#000',
+    textAlign: 'center',
+    padding: 30,
+  },
+
 });
